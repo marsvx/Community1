754f797e437900859b8d675227a319e3c22dde84
Goku900 <lizeth.a.nava@outlook.com>
2024-10-02 01:38:51 -0600

    made changes to users migrate file to create index for email

--- db/migrate/20240929230309_create_users.rb
+++ db/migrate/20240929230309_create_users.rb
@@ -2,11 +2,15 @@ class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users, id:false do |t|
       t.string :username, null: false, limit: 15, primary_key: true
-      t.string :email, null: false, limit: 255, unique: true
+      t.string :email, null: false, limit: 255
       t.string :firstName, null: false, limit: 150
       t.string :ageRange, null: false, limit: 8
       t.string :hashedPasswd, null: false, limit: 80
       t.integer :zipCode
     end
+     # unique index for email, not that this is not necessary for username bc an index gets created automatically
+     # due to username being a primary_key
+     add_index :users, :email, unique: true
+
   end
 end
