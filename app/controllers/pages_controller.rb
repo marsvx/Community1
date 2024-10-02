6c9bc80a1edbc569c9a77c65258b5d0d7e36edd7
Goku900 <lizeth.a.nava@outlook.com>
2024-10-01 21:58:13 -0600

    Corrected database name in .env file and in database.yml

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
+    @events = Event.where(status: "approved")
+      .where(date: Date.today.beginning_of_week..Date.today.end_of_week)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "pages#home"
   get 'pages/home'
   resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
