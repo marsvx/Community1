29c9ffaf08506d297011f8d57c443b8347c57d5a
Goku900 <lizeth.a.nava@outlook.com>
2024-10-04 03:15:40 -0600

    created has_many relation under organization for event

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,5 @@
 class Organization < ApplicationRecord
-    has_many : classifications, class_name: 'Classification'
-    has_many : categories, class_name: 'Category', through: :classifications
+    has_many :classifications, class_name: 'Classification'
+    has_many :categories, class_name: 'Category', through: :classifications
+    has_many :events, class_name: 'Event'
 end
