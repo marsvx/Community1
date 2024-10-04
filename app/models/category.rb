78c097e06a4b43242744e239a5efd446c8aa0b2e
Goku900 <lizeth.a.nava@outlook.com>
2024-10-04 03:06:26 -0600

    created has_many : :though relations between Category, Organization, and Classification

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+    has_many :classifications, class_name: 'Classification'
+    has_many :organizations, class_name: 'Organization', through: :classification
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,4 @@
 class Organization < ApplicationRecord
     has_many : classifications, class_name: 'Classification'
+    has_many : categories, class_name: 'Category', through: :classifications
 end
