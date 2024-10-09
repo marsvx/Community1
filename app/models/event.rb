d2eb1eddec2012a78fd8f3843e19bc5de673bf91
Goku900 <lizeth.a.nava@outlook.com>
2024-10-09 16:22:52 -0600

    saving,norendering errors

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
     belongs_to :organization
     belongs_to :user
     belongs_to :admin
+
+    def location
+        "#{street}, #{city}, #{eventState}, #{zipcode}"
+      end
 end
