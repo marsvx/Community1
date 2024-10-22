339e3f6d5130396f06be3742b08d25df3054e7cd
Maria Estrada <119823752+Ariamestra@users.noreply.github.com>
2024-10-22 11:16:25 -0600

    Made explore page websites to connect to database (#66)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,20 +4,26 @@ class PagesController < ApplicationController
   end
 
   def food
+    @organizations = Organization.all
   end
 
   def housing
+    @organizations = Organization.all
   end
 
   def healthcare
+    @organizations = Organization.all
   end
 
   def education
+    @organizations = Organization.all
   end
 
   def childcare
+    @organizations = Organization.all
   end
 
   def legal_resources
+    @organizations = Organization.all
   end
 end
